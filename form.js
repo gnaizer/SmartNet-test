b5c43a7d87a351ea4a5f72e386ea67363041e612
gnaizer <gntnaizer@gmail.com>
2018-02-10 18:58:28 +0200

    form3

--- form.js
+++ form.js
@@ -1,20 +1,22 @@
 $(function() {
   $('form input[type=submit]').click(function(e) {
-      const form = $('form')[0];
-      if (!form.checkValidity()) {
-          return alert( 'Success!' );
-      }
-      e.preventDefault();
-      $.ajax({
-          url: "https://formspree.io/gntnaizer@gmail.com",
-          method: "POST",
-          data: { 
-              Fullname: $('#name').val(),
-              Email: $('#email').val(),
-              Issues: $('#sel1').val()},
-          dataType: "json"
-      });
+    const form = $('form')[0];
+    if (!form.checkValidity()) {
+      return
+    }
+    e.preventDefault();
+    $.ajax({
+      url: "https://formspree.io/gntnaizer@gmail.com",
+      method: "POST",
+      data: {
+        Fullname: $('#name').val(),
+        Email: $('#email').val(),
+        Issues: $('#sel1').val()
+      },
+      dataType: "json"
+    });
   });
-
 });
+
+
 
--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-$(document).ready(function(){
-  $('#btnClear').click(function(){				
-    if(confirm("Want to clear?")){
-      /*Clear all input type="text" box*/
+// Clear button
+$(document).ready(function() {
+  $('#btnClear').click(function() {
+    if (confirm("Want to clear?")) {
       $('#form1 input[type="text"]').val('');
       $('#form1 #sel1').val('');
-    }					
+    }
   });
 });
+
+
+// Set image
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#background')
+        .attr('src', e.target.result);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+
+// Disable submit
+$(function() {
+  $('#name, #email, sel1').keyup(function() {
+    if ($(this).val() == '') {
+      $('.enableOnInput').prop('disabled', true);
+    } else {
+      $('.enableOnInput').prop('disabled', false);
+    }
+  });
+});
+
+
+// Refresh after submit
+// function refresh() {
+//   setTimeout(function () {
+//       location.reload()
+//   }, 100);
+// }
+// alert ( 'Congratulation! Form allready sended');
