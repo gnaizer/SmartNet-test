c11c4a7bd4484e8506ae1b40a53e14d5a8f074d6
gnaizer <gntnaizer@gmail.com>
2018-02-10 17:01:54 +0200

    form1

--- form.js
+++ form.js
@@ -2,16 +2,16 @@ $(function() {
   $('form input[type=submit]').click(function(e) {
       const form = $('form')[0];
       if (!form.checkValidity()) {
-          return;
+          return alert( 'Success!' );
       }
       e.preventDefault();
       $.ajax({
           url: "https://formspree.io/gntnaizer@gmail.com",
           method: "POST",
           data: { 
-              Fullname: $('#name').val(),
-              Emailaddress: $('#email').val(),
-                Issues: $('#sel1').val()},
+              name: $('#name').val(),
+              email: $('#email').val(),
+              sel1: $('#sel1').val()},
           dataType: "json"
       });
   });
